af8f6204851c2870e47fdf9ea9ad13641eae2611
NetizenDellius <lukas.dellius@gmail.com>
2026-05-04 11:43:25 +0200

    keine ahnung, hilfe.

--- toolkit.py
+++ toolkit.py
@@ -1,9 +1,9 @@
 # Mini-Toolkit – bitte NICHT am Header ändern, außer wenn die
 #Aufgabe es verlangt!
-#VERSION = "0.1"
+#VERSION = "0.2"
 #def banner():
 # >>> KONFLIKTBEREICH: Hier werden zwei Personen
 #gleichzeitig ändern <<<
-#return "Mini-Toolkit v0.1 - Welcome"
+#return "Mini-Toolkit v0.2 - Willkommen vom Team D""
 #if __name__ == "__main__":
 #    print(banner())
