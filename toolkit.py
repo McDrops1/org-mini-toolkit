96ac6c6f41f1df530cb54c0fa84cf4b8d32993b3
Marc von Fintel <von.fintel@gmx.de>
2026-05-04 11:09:14 +0200

    feat: banner-upgrade

--- toolkit.py
+++ toolkit.py
@@ -1,9 +1,10 @@
 # Mini-Toolkit – bitte NICHT am Header ändern, außer wenn die
 #Aufgabe es verlangt!
 #VERSION = "0.1"
-#def banner():
+def banner():
 # >>> KONFLIKTBEREICH: Hier werden zwei Personen
 #gleichzeitig ändern <<<
-#return "Mini-Toolkit v0.1 - Welcome"
-#if __name__ == "__main__":
-#    print(banner())
+    return "Mini-Toolkit v0.2 - Hello from Team C"
+
+if __name__ == "__main__":
+    print(banner())
