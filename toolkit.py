7cab5625d3d8d185903870fd569074a1b2680def
NetizenDellius <lukas.dellius@gmail.com>
2026-05-04 11:46:49 +0200

    de-kommentiert

--- toolkit.py
+++ toolkit.py
@@ -1,9 +1,9 @@
-# Mini-Toolkit – bitte NICHT am Header ändern, außer wenn die
+#Mini-Toolkit – bitte NICHT am Header ändern, außer wenn die
 #Aufgabe es verlangt!
-#VERSION = "0.2"
-#def banner():
-# >>> KONFLIKTBEREICH: Hier werden zwei Personen
+VERSION = "0.2"
+def banner():
+#>>> KONFLIKTBEREICH: Hier werden zwei Personen
 #gleichzeitig ändern <<<
-#return "Mini-Toolkit v0.2 - Willkommen vom Team D""
-#if __name__ == "__main__":
-#    print(banner())
+    return "Mini-Toolkit v0.2 - Willkommen vom Team D"
+    if __name__ == "__main__":
+        print(banner())
